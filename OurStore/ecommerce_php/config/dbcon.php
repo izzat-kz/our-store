f594892e2bb84003dfdc26a9f56955ae7cda0ca9
izzat-kz <m.izzatkz@gmail.com>
2024-01-17 09:57:57 +0800

    Update dbcon.php

--- OurStore/ecommerce_php/config/dbcon.php
+++ OurStore/ecommerce_php/config/dbcon.php
@@ -5,10 +5,8 @@
      $password = "";
      $database = "project";
 
-     // Creating database connection
      $con = mysqli_connect("$host", "$username", "$password", "$database");
      
-     // Check database connection
      if(!$con)
      {
          die("Connection Failed: ".mysqli_connect_error());
